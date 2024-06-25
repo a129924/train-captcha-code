bf98fa329595bb72c0fe6677e1ec5fde03a45fa7
a129924 <a129924@gmail.com>
2024-06-25 16:06:37 +0800

    feat : 移除不需要的function write_verification_code_pic 更動邏輯

--- src/train_verification_code/service/verification_code_pic_service.py
+++ src/train_verification_code/service/verification_code_pic_service.py
@@ -1,30 +1,25 @@
 from collections.abc import Generator
 
+from ..project_typing._typing import WriteFileState
 from ..schema.verification_code_response import VerificationCodeResponse
-from ..utils.fetch import fetch_url
+from ..utils.fetch import fetch_url_to_json
 
 
 def fetch_verification_code_url(url: str) -> VerificationCodeResponse:
-    return VerificationCodeResponse(**fetch_url(url))
-
-
-def get_verification_code_pic_content(
-    verification_code_response: VerificationCodeResponse,
-) -> Generator[tuple[str, str], None, None]:
-    return (
-        (codelist.ans, fetch_url(f"data:image/png;base64,{codelist.code}"))
-        for codelist in verification_code_response.codelist
-    )
+    return VerificationCodeResponse(**fetch_url_to_json(url))
 
 
 def write_verification_code_pic(
-    contents: Generator[tuple[str, str], None, None],
-) -> None:
+    cverification_code_response: VerificationCodeResponse,
+) -> Generator[WriteFileState, None, None]:
     from os.path import join
 
-    from ..utils.file import write_file
+    from ..utils.file import write_file_from_byte_string
 
     root = "./verification_code_pic"
 
-    for ans, content in contents:
-        write_file(text=content, out_filapath=join(root, f"{ans}.png"))
+    for codelist in cverification_code_response.codelist:
+        yield from write_file_from_byte_string(
+            text=codelist.code,
+            out_filapath=join(root, f"{codelist.ans}.png"),
+        )
