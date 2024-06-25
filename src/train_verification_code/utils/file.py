23654458302dc27aa1067fcc5da0e0b2481a4b76
a129924 <a129924@gmail.com>
2024-06-25 16:08:30 +0800

    feat : 更動撰寫方式 減少階層

--- src/train_verification_code/utils/file.py
+++ src/train_verification_code/utils/file.py
@@ -1,9 +1,23 @@
+from collections.abc import Generator
+
+from ..project_typing._typing import WriteFileState
+
+
 def write_file_from_byte_string(
     text: str,
     out_filapath: str,
     mode: str = "wb",
-) -> None:
+) -> Generator[WriteFileState, None, None]:
     from base64 import b64decode
 
-    with open(out_filapath, mode=mode) as file:
+    try:
+        file = open(out_filapath, mode=mode)
         file.write(b64decode(text.split(",", 1)[1]))
+
+        yield WriteFileState(filepath=out_filapath, state="success")
+
+    except Exception:
+        yield WriteFileState(filepath=out_filapath, state="failed")
+
+    finally:
+        file.close()
