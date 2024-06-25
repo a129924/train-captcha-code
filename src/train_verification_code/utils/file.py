6f5a223c0949bae0ce8e6138e62710dd64590f59
a129924 <a129924@gmail.com>
2024-06-25 15:13:14 +0800

    fix : 修改byte string 不能直接寫入 要用base64.b64decode decode才能正常寫入

--- src/train_verification_code/utils/file.py
+++ src/train_verification_code/utils/file.py
@@ -1,8 +1,9 @@
-def write_file(
+def write_file_from_byte_string(
     text: str,
     out_filapath: str,
-    mode: str = "w+",
-    encoding: str = "UTF-8",
+    mode: str = "wb",
 ) -> None:
-    with open(out_filapath, mode=mode, encoding=encoding) as file:
-        file.write(text)
+    from base64 import b64decode
+
+    with open(out_filapath, mode=mode) as file:
+        file.write(b64decode(text.split(",", 1)[1]))
