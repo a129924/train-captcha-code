6014f2d0fd0dd204966b71f3d4a6e71efdafd460
a129924 <a129924@gmail.com>
2024-06-27 10:44:29 +0800

    refactor : 從寫死 換成 參數塞入

--- src/train_verification_code/service/captcha_code_pic_service.py
+++ src/train_verification_code/service/captcha_code_pic_service.py
@@ -11,27 +11,25 @@ def fetch_verification_code_url(url: str) -> VerificationCodeResponse:
 
 
 def write_verification_code_pics(
+    root_path: str,
     cverification_code_response: VerificationCodeResponse,
 ) -> Generator[WriteFileState, None, None]:
-    from ..utils.sync.file import write_file_from_byte_string
-
-    root = "./verification_code_pic"
+    from ..utils.sync._file import write_file_from_byte_string
 
     for codelist in cverification_code_response.codelist:
         yield write_file_from_byte_string(
             text=codelist.code,
-            out_filapath=join(root, f"{codelist.ans}.png"),
+            out_filapath=join(root_path, f"{codelist.ans}.png"),
         )
 
 
 async def async_write_verification_code_pic(
+    root_path: str,
     text: str,
     filename: str,
 ) -> WriteFileState:
-    from ..utils._async.file import write_file_from_byte_string
-
-    root = "./verification_code_pic"
+    from ..utils._async._file import write_file_from_byte_string
 
     return await write_file_from_byte_string(
-        text=text, out_filapath=join(root, filename)
+        text=text, out_filapath=join(root_path, filename)
     )
