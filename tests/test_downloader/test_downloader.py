2f33d0312650c032db34e7d787e78f348e96907c
a129924 <a129924@gmail.com>
2024-07-01 00:03:19 +0800

    test : 修改參數

--- tests/test_downloader/test_downloader.py
+++ tests/test_downloader/test_downloader.py
@@ -8,15 +8,22 @@ async def test_500_downloader():
     count = 500
     downloader = CaptchaCodeDownloader(
         config_path="./config/download.env",
-        limit_file_count=count,
+        target_quantity=count,
+        file_extend=".png",
     )
 
     amount = 0
 
-    async for states in downloader.pipeline():
+    states = await downloader.pipeline()
+
+    if states:
         for state in states:
             amount += 1
 
             assert state["state"] == "success"
 
-    assert amount >= count
+        assert amount >= count
+
+    # assert downloader.remaining_quantity == 0
+    # assert downloader.config == ""
+    # assert states is not None
