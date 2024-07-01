083c5b9a46ce4c67734a9c5017230928e9898f0f
a129924 <a129924@gmail.com>
2024-07-01 16:22:14 +0800

    feat : 撈取目標資料夾下的所有檔案 也可以依照file_extension 限定副檔名

--- src/train_captcha_code/project_typing/_typing.py
+++ src/train_captcha_code/project_typing/_typing.py
@@ -1,8 +1,13 @@
 from typing import Literal
 
-from typing_extensions import TypedDict
+from typing_extensions import NamedTuple, TypedDict
 
 
 class WriteFileState(TypedDict):
     filepath: str
     state: Literal["success", "failed"]
+
+
+class LabeledFile(NamedTuple):
+    target_file: str
+    label: str
--- src/train_captcha_code/utils/utils.py
+++ src/train_captcha_code/utils/utils.py
@@ -1,4 +1,19 @@
+from typing import Optional
+
+
 def calculate_max_quantity(max_count: int, base_num: int) -> int:
     from math import ceil
 
     return ceil(max_count / base_num)
+
+
+def get_files_in_folder(
+    path: str,
+    file_extension: Optional[str] = None,
+) -> list[str]:
+    from os import listdir
+
+    if file_extension:
+        return [file for file in listdir(path=path) if file.endswith(file_extension)]
+    else:
+        return listdir(path=path)
